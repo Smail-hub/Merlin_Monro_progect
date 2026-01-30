f1cdc7fd4fc0fd365f18a7422aa262372cf939c1
Smail-hub <amilsafin@gmail.com>
2026-01-30 13:12:14 +0500

    Вроде всё
    Убрал все лишнее

---
+++ MerlinMonro.py
@@ -0,0 +1,69 @@
+from PIL import Image
+
+image = Image.open("monro.jpg")
+
+red, green, blue = image.split() 
+
+red_channel = red
+green_channel = green
+blue_channel = blue
+
+new_image = Image.merge("RGB", (red_channel, green_channel, blue_channel))
+
+image1 = red_channel
+image2 = red_channel
+
+
+cmyk_image1 = image1.convert("CMYK")
+
+cmyk_image2 = image2.convert("CMYK")
+
+coordinates = (101, 0, cmyk_image1.width, cmyk_image1.height)
+cropped_left = cmyk_image1.crop(coordinates)
+
+coordinates = (51, 0, cmyk_image1.width - 50, cmyk_image1.height)
+cropped_middle = cmyk_image2.crop(coordinates)
+
+
+image3 = Image.blend(cropped_left, cropped_middle, 0.3)
+
+red_channel_displaced = image3
+
+image1 = blue_channel
+image2 = blue_channel
+
+cmyk_image1 = image1.convert("CMYK")
+cmyk_image2 = image2.convert("CMYK")
+
+coordinates = (101, 0, cmyk_image1.width, cmyk_image1.height)
+cropped_left = cmyk_image1.crop(coordinates)
+
+coordinates = (51, 0, cmyk_image1.width - 50, cmyk_image1.height)
+cropped_middle = cmyk_image2.crop(coordinates)
+
+image3 = Image.blend(cropped_left, cropped_middle, 0.7)
+
+blue_channel_dicplaced = image3
+
+image1 = green_channel
+cmyk_image1 = image1.convert("CMYK")
+
+coordinates = (71, 0, cmyk_image1.width - 30, cmyk_image1.height)
+cropped_middle = cmyk_image1.crop(coordinates)
+
+green_channel_cropped_middle = cropped_middle
+
+red = red_channel_displaced
+green = green_channel_cropped_middle  
+blue = blue_channel_dicplaced
+
+red = red.convert('L')
+green = green.convert('L')
+blue = blue.convert('L')
+
+rgb_image = Image.merge("RGB", (red, green, blue))
+
+thumbnail_image = rgb_image.copy()
+
+thumbnail_image.thumbnail((80, 80))
+thumbnail_image.save("MerlinMonro_finish.jpg")
