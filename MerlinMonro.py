71ee97b93e7791882e21c6c902c1c4d06133c24a
Smail-hub <amilsafin@gmail.com>
2026-01-30 13:44:07 +0500

    Исправил косяк, теперь синий канал смещен вправо

--- MerlinMonro.py
+++ MerlinMonro.py
@@ -25,7 +25,7 @@ coordinates = (51, 0, cmyk_image1.width - 50, cmyk_image1.height)
 cropped_middle = cmyk_image2.crop(coordinates)
 
 
-image3 = Image.blend(cropped_left, cropped_middle, 0.3)
+image3 = Image.blend(cropped_left, cropped_middle, 0.5)
 
 red_channel_displaced = image3
 
@@ -35,13 +35,13 @@ image2 = blue_channel
 cmyk_image1 = image1.convert("CMYK")
 cmyk_image2 = image2.convert("CMYK")
 
-coordinates = (101, 0, cmyk_image1.width, cmyk_image1.height)
-cropped_left = cmyk_image1.crop(coordinates)
+coordinates = (0, 0, cmyk_image1.width - 101, cmyk_image1.height)
+cropped_right = cmyk_image1.crop(coordinates)
 
 coordinates = (51, 0, cmyk_image1.width - 50, cmyk_image1.height)
 cropped_middle = cmyk_image2.crop(coordinates)
 
-image3 = Image.blend(cropped_left, cropped_middle, 0.7)
+image3 = Image.blend(cropped_right, cropped_middle, 0.5)
 
 blue_channel_dicplaced = image3
 
