8d18c6d361aa6ec894f8a41753ae2ef8e9c064ad
Smail-hub <amilsafin@gmail.com>
2026-02-05 14:59:46 +0500

    Ещё высушил код, удалил лишнее

--- MerlinMonro.py
+++ MerlinMonro.py
@@ -2,53 +2,39 @@ from PIL import Image
 
 image = Image.open("monro.jpg")
 
-red, green, blue = image.split() 
+red, green, blue = image.split()
 
-new_image = Image.merge("RGB", (red, green, blue))
+image = red
 
-image1 = red
-image2 = red
+coordinates = (100, 0, image.width, image.height)
+cropped_left = image.crop(coordinates)
 
-coordinates = (100, 0, image1.width, image1.height)
-cropped_left = image1.crop(coordinates)
+coordinates = (50, 0, image.width - 50, image.height)
+cropped_middle = image.crop(coordinates)
 
-coordinates = (50, 0, image1.width - 50, image1.height)
-cropped_middle = image2.crop(coordinates)
+red_channel_displaced = Image.blend(cropped_left, cropped_middle, 0.5)
 
-image3 = Image.blend(cropped_left, cropped_middle, 0.5)
-red_channel_displaced = image3
+image = blue
 
-image1 = blue 
-image2 = blue
+coordinates = (0, 0, image.width - 100, image.height)
+cropped_right = image.crop(coordinates)
 
-coordinates = (0, 0, image1.width - 100, image1.height)
-cropped_right = image1.crop(coordinates)
+coordinates = (50, 0, image.width - 50, image.height)
+cropped_middle = image.crop(coordinates)
 
-coordinates = (50, 0, image1.width - 50, image1.height)
-cropped_middle = image2.crop(coordinates)
+blue_channel_displaced = Image.blend(cropped_right, cropped_middle, 0.5)
 
-image3 = Image.blend(cropped_right, cropped_middle, 0.5)
+image = green
 
-blue_channel_dicplaced = image3
-
-image1 = green
-
-coordinates = (70, 0, image1.width - 30, image1.height)
-cropped_middle = image1.crop(coordinates)
-
-green_channel_cropped_middle = cropped_middle
+coordinates = (50, 0, image.width - 50, image.height)
+green_channel_cropped_middle = image.crop(coordinates)
 
 red = red_channel_displaced
-green = green_channel_cropped_middle  
-blue = blue_channel_dicplaced
-
-red = red.convert('L')
-green = green.convert('L')
-blue = blue.convert('L')
+green = green_channel_cropped_middle
+blue = blue_channel_displaced
 
 rgb_image = Image.merge("RGB", (red, green, blue))
 
 thumbnail_image = rgb_image.copy()
-
 thumbnail_image.thumbnail((80, 80))
 thumbnail_image.save("MerlinMonro_finish.jpg")
