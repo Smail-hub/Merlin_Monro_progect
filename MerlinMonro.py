fcdf59701af1bc08d679b44dab150d3f81cce3b6
Smail-hub <amilsafin@gmail.com>
2026-02-05 22:37:16 +0500

    скрипт перестал работать

--- MerlinMonro.py
+++ MerlinMonro.py
@@ -4,37 +4,27 @@ image = Image.open("monro.jpg")
 
 red, green, blue = image.split()
 
-image = red
-
-coordinates = (100, 0, image.width, image.height)
+coordinates = (100, 0, red.width, red.height)
 cropped_left = image.crop(coordinates)
 
-coordinates = (50, 0, image.width - 50, image.height)
+coordinates = (50, 0, red.width - 50, red.height)
 cropped_middle = image.crop(coordinates)
 
 red_channel_displaced = Image.blend(cropped_left, cropped_middle, 0.5)
 
-image = blue
-
-coordinates = (0, 0, image.width - 100, image.height)
+coordinates = (0, 0, blue.width - 100, blue.height)
 cropped_right = image.crop(coordinates)
 
-coordinates = (50, 0, image.width - 50, image.height)
+coordinates = (50, 0, blue.width - 50, blue.height)
 cropped_middle = image.crop(coordinates)
 
 blue_channel_displaced = Image.blend(cropped_right, cropped_middle, 0.5)
 
-image = green
-
-coordinates = (50, 0, image.width - 50, image.height)
+coordinates = (50, 0, green.width - 50, green.height)
 green_channel_cropped_middle = image.crop(coordinates)
 
-red = red_channel_displaced
-green = green_channel_cropped_middle
-blue = blue_channel_displaced
+thumbnail_image = Image.merge("RGB", (red, green, blue))
+thumbnail_image.save("thumbnail_image.jpg")
 
-rgb_image = Image.merge("RGB", (red, green, blue))
-
-thumbnail_image = rgb_image.copy()
 thumbnail_image.thumbnail((80, 80))
 thumbnail_image.save("MerlinMonro_finish.jpg")
